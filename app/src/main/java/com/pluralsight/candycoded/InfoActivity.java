a49dc104350e32a7ebc6e4f05db3808641c82e65
mohale morena <morenamohale@gmail.com>
2020-09-30 17:31:02 +0200

    Second Commit

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -22,10 +24,12 @@ public class InfoActivity extends AppCompatActivity {
 
 
     }
-
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
+    public void createMapIntent(View view){
+        Intent mapIntent = new Intent ( Intent.ACTION_VIEW, Uri.parse ( "geo:0,0?q=618 E South St Orlando, FL 32801" ) );
+        if (mapIntent.resolveActivity ( getPackageManager () ) != null)
+        startActivity ( mapIntent );
+        mapIntent.setPackage("com.google.android.apps.maps");
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
